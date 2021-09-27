1bdef96dbd599a2ecadd118678d1cc3418fb2f61
Jeon sumin <sumin1236@naver.com>
2021-09-28 02:08:03 +0900

    login&regist

--- app/src/main/java/com/hanyang/login/RegisterActivity.java
+++ app/src/main/java/com/hanyang/login/RegisterActivity.java
@@ -57,7 +57,8 @@ public class RegisterActivity extends AppCompatActivity {
                             }else{
                                 Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
                             }
-                        } catch(Exception t){ Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_LONG).show();
+                        } catch(Exception t){
+                            Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_LONG).show();
                         }
                     }
                 }, new Response.ErrorListener(){
